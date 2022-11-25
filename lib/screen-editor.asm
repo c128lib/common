0574fab4fefb2ef58a58caa79b65df0a24e11cdd
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2022-11-25 22:14:40 +0100

    Screen editor labels wip

--- lib/screen-editor.asm
+++ lib/screen-editor.asm
@@ -1,9 +1,19 @@
 /*
  * Requires KickAssembler v5.x
  * (c) 2022 Raffaele Intorcia
+ *
+ * References available at
+ * https://c128lib.github.io/Reference/C000
  */
 #importonce
 .filenamespace c128lib
 
-.label WRITE_VDC = $CDCC
-.label READ_VDC  = $CDDA
+.label CINT           = $C000
+.label DISPLY         = $C003
+.label KEYIN          = $C006
+.label GETSCRN        = $C009
+.label PRINT          = $C00C
+.label SCRORG         = $C00F
+
+.label WRITE_VDC      = $CDCC
+.label READ_VDC       = $CDDA

a6c8adffa69016b0b6d76b27720ba70cd684e7df
Raffaele Intorcia <raffaele.intorcia@gmail.com>
2022-11-26 17:55:21 +0100

    Screen editor wip

--- lib/screen-editor.asm
+++ lib/screen-editor.asm
@@ -10,25 +10,36 @@
 
 .namespace ScreenEditor {
 
-.label CINT           = $C000
-.label DISPLY         = $C003
-.label KEYIN          = $C006
-.label GETSCRN        = $C009
-.label PRINT          = $C00C
-.label SCRORG         = $C00F
-.label SCNKEY         = $C012
-.label REPEAT         = $C015
-.label PLOT           = $C018
-.label CRSR80         = $C01B
-.label ESCAPE         = $C01E
-.label KEYSET         = $C021
-.label SCNIRQ         = $C024
-.label INIT80         = $C027
-.label SWAPPER        = $C02A
-.label WINDOW         = $C02D
+.label JCINT          = $C000
+.label JDISPLY        = $C003
+.label JKEYIN         = $C006
+.label JGETSCRN       = $C009
+.label JPRINT         = $C00C
+.label JSCRORG        = $C00F
+.label JSCNKEY        = $C012
+.label JREPEAT        = $C015
+.label JPLOT          = $C018
+.label JCRSR80        = $C01B
+.label JESCAPE        = $C01E
+.label JKEYSET        = $C021
+.label JSCNIRQ        = $C024
+.label JINIT80        = $C027
+.label JSWAPPER       = $C02A
+.label JWINDOW        = $C02D
 .label SADDRTBL       = $C033
 .label SCNVCTRS       = $C065
 .label KEYPTRS        = $C06F
+.label CINT           = $C07B
+.label CLEAR          = $C142
+.label HOME           = $C150
+.label SETLINE        = $C15C
+.label SETADDR        = $C15E
+.label SCNIRQ         = $C194
+.label KEYIN          = $C234
+.label KEYLIN         = $C258
+.label GETSCRN        = $C29B
+.label QUOTECK        = $C2FF
+.label PRNTEXIT       = $C30C
 
 
 .label WRITE_VDC      = $CDCC
